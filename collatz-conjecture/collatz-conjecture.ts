10c1bef6edf197b08a6f251fd8edc3efb8ef5784
Pho Huynh <pho.huynh@orientsoftware.com>
2019-10-22 15:45:09 +0700

    Resolve collatz conjecture

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,4 +1,25 @@
 class CollatzConjecture {
+    
+    public static steps(input: number): number {
+        if (input < 1) {
+            throw Error('Only positive numbers are allowed');
+        }
+        
+        let output = input;
+        let step = 0;
+
+        while(output !== 1) {
+            if (output % 2 === 0) {
+                output = output / 2;
+            } else {
+                output = output * 3 + 1;
+            }
+
+            step++;
+        }
+
+        return step;
+    }
 }
 
 export default CollatzConjecture
